215ae9c0b03b4c6bf01d277e0c324f1b241d0b4c
unknown <alexfung1234@gmail.com>
2022-11-29 19:00:50 +0800

    book display

--- pages/Art/index.js
+++ pages/Art/index.js
@@ -11,7 +11,7 @@ export default function Art() {
  return <div className='bookContainer'>{
   BookArray.map((book) => {
    return (
-    <Link href={'/Art/' + book.id} className="bookDisplay">{book.name}{book.author}</Link>)
+    <Link href={'/Art/' + book.id} className="bookDisplay"><img src={`/${book.img}.jpg`} width={300} height={350} />{book.name}{book.author}</Link>)
   })
 
  }</div>
