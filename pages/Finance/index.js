b5ed9bc10ca0e7974406eaa70fa3df591f900b5d
unknown <alexfung1234@gmail.com>
2022-11-30 18:43:09 +0800

    fix

--- pages/Finance/index.js
+++ pages/Finance/index.js
@@ -6,17 +6,17 @@ const BookArray = Book.Books
 
 export default function Finance() {
 
- return <div className='bookContainer'>{
-  BookArray.map((book) => {
-   return (
-    <Link href={'/Finance/' + book.id} className="bookDisplay">
-     <img src={`/${book.img}.jpg`} width={300} height={350} className="bookImage" />
-     <div className='bookTitle'>{book.name}</div>
-     <div className='bookAuthor'>{book.author}</div>
-     <div className='bookPrice'>現價: HK{book.price}.00</div>
-    </Link>)
-  })
+  return <div className='bookContainer'>{
+    BookArray.map((book) => {
+      return (
+        <Link href={'/Finance/' + book.id} className="bookDisplay">
+          <img src={`/${book.img}.jpg`} width={300} height={350} className="bookImage" />
+          <div className='bookTitle'>{book.name}</div>
+          <div className='bookAuthor'>{book.author}</div>
+          <div className='bookPrice'>現價: HK{book.price}.00</div>
+        </Link>)
+    })
 
- }</div>
+  }</div>
 }
 
