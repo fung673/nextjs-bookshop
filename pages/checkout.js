35a3bcfd3d2fd1e741ee167e220b3b18e4f2af0c
unknown <alexfung1234@gmail.com>
2023-02-28 18:59:59 +0800

    checkout page 80%

--- pages/checkout.js
+++ pages/checkout.js
@@ -2,9 +2,17 @@ import React from "react";
 import { CartContext } from "../components/CartContext"
 import styles from '../styles/checkout.module.css'
 import { useContext } from "react";
+import { useState } from "react";
 
 export default function Checkout() {
  const { cartItems, setCartItems } = useContext(CartContext)
+ const [data, setData] = useState({
+  Name: "",
+  Email: "",
+  Mobile: "",
+  Address: ""
+
+ })
 
  function multiple(a, b) {
   return a * b
@@ -14,16 +22,32 @@ export default function Checkout() {
   return accumulator + value;
  }, 0);
 
+ const handleChange = (event) => {
+  const { name, value } = event.target
+  setData(prev => ({ ...prev, [name]: value }))
+  console.log(data)
+ }
 
+
+ function checkInfo() {
+  for (const key in data) {
+   if (data[key] == "") {
+    alert(`${key} is missing`)
+    console.log(data[key])
+    return -1
+   }
+  } alert("成功下單")
+ }
 
  return <div className={styles.main}>
   <div className={styles.left}>
    <h2 className={styles.title}>結帳</h2>
    <div className={styles.list}>
-    <input className={styles.input} placeholder=" Name *" />
-    <input className={styles.input} placeholder=" Email *" />
-    <input className={styles.input} placeholder=" Mobile *" />
-    <textarea className={styles.input2} placeholder=" Address *" />
+    <input className={styles.input} placeholder=" Name *" name="Name" onChange={handleChange} />
+    <input className={styles.input} placeholder=" Email *" name="Email" onChange={handleChange} />
+    <input className={styles.input} placeholder=" Mobile *" name="Mobile" onChange={handleChange} />
+    <textarea className={styles.input2} placeholder=" Address *" name="Address" onChange={handleChange} />
+    <button className={styles.button} onClick={checkInfo}>下單</button>
    </div>
   </div>
   <div className={styles.right}><div className={styles.orderSummary}>
